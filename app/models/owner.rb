a38d326b51c25da94c57ac61f5116424069a258e
laurakathleen <laurakwalley@gmail.com>
2016-10-03 17:13:56 -0700

    gets validations working

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,8 @@
 class Owner < ActiveRecord::Base
   # TODO: add validations
+  validates :first_name, :last_name, :email, presence: true, length: {maximum: 255}
+  validates :email, uniqueness: true, format: { with: /\A[@]+\z/, message: "email must contain @" }
+  has_many :line_items, inverse_of: :order
 
   before_save :normalize_phone_number
 
